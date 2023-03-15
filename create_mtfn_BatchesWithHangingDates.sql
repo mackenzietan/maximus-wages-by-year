48292f246e5f5c7fca3fccb8b8d2cdedd5429b5b
Mac Tantlinger <mactantlinger@gmail.com>
2023-03-15 12:04:17 -0400

    added year calc and reformatted datatypes in batches w hanging dates fn

--- create_mtfn_BatchesWithHangingDates.sql
+++ create_mtfn_BatchesWithHangingDates.sql
@@ -8,7 +8,7 @@ SET QUOTED_IDENTIFIER ON
 GO
 
 CREATE FUNCTION [dbo].[mtfn_BatchesWithHangingDates] (
-    @TargetYear int = NULL
+    @TargetYear INT = NULL
 )
 RETURNS @BatchesWithHangingDates TABLE (
     PayBatchID INT PRIMARY KEY
@@ -20,20 +20,23 @@ RETURNS @BatchesWithHangingDates TABLE (
 )
 AS
 BEGIN
-    DECLARE @StartDateVC varchar(30)
-    DECLARE @EndDateVC varchar(30)
-    DECLARE @EndDateVC2 varchar(30)
+    DECLARE @YearPlusOne INT
+    DECLARE @StartDateVC VARCHAR(30)
+    DECLARE @EndDateVC VARCHAR(30)
+    DECLARE @EndDateVC2 VARCHAR(30)
     DECLARE @StartDateDT DATETIME
     DECLARE @EndDateDT DATETIME
     DECLARE @EndDateDT2 DATETIME
 
-    SET @StartDateVC = CONVERT(varchar(4), @TargetYear) + '-01-01 00:00:00.000'
-    SET @EndDateVC = CONVERT(varchar(4), @TargetYear) + '-12-31 23:59:59.999'
-    SET @EndDateVC2 = CONVERT(varchar(4), @TargetYear+1) + '-01-14 00:00:00.000'
+    SET @YearPlusOne = @TargetYear+1
 
-    SET @StartDateDT = CONVERT(datetime, @StartDateVC)
-    SET @EndDateDT = CONVERT(datetime, @EndDateVC)
-    SET @EndDateDT2 = CONVERT(datetime, @EndDateVC2)
+    SET @StartDateVC = CONVERT(VARCHAR(4), @TargetYear) + '-01-01 00:00:00.000'
+    SET @EndDateVC = CONVERT(VARCHAR(4), @TargetYear) + '-12-31 23:59:59.999'
+    SET @EndDateVC2 = CONVERT(VARCHAR(4), @YearPlusOne) + '-01-14 00:00:00.000'
+
+    SET @StartDateDT = CONVERT(DATETIME, @StartDateVC)
+    SET @EndDateDT = CONVERT(DATETIME, @EndDateVC)
+    SET @EndDateDT2 = CONVERT(DATETIME, @EndDateVC2)
     BEGIN
         INSERT INTO @BatchesWithHangingDates --lists pay batches with hanging dates at the beginning of the year
         SELECT 
@@ -44,7 +47,7 @@ BEGIN
             ,EndDate
             ,uc.UTDate
         FROM dbo.PayBatchDefinition AS pbd
-        JOIN dbo.UtilityCalendar AS uc ON (uc.UTDate between pbd.StartDate and pbd.EndDate)
+        JOIN dbo.UtilityCalendar AS uc ON (uc.UTDate BETWEEN pbd.StartDate AND pbd.EndDate)
         WHERE PayYear = @TargetYear         --selects any pay batch in 20xx...
             AND StartDate < @StartDateDT    -- where the start date is before 20xx-01-01
             AND BatchNumber > 19999999      --only gives us batches/dates in the 20xxmmdd format
@@ -59,8 +62,8 @@ BEGIN
             ,EndDate
             ,uc.UTDate
         FROM dbo.PayBatchDefinition AS pbd
-        JOIN dbo.UtilityCalendar AS uc ON (uc.UTDate between pbd.StartDate and pbd.EndDate)
-        WHERE PayYear IN (@TargetYear,@TargetYear+1)    --selects any pay batch in 20xx and 20xx+1...
+        JOIN dbo.UtilityCalendar AS uc ON (uc.UTDate BETWEEN pbd.StartDate AND pbd.EndDate)
+        WHERE PayYear IN (@TargetYear,@YearPlusOne)     --selects any pay batch in 20xx and 20xx+1...
             AND EndDate > @EndDateDT                    -- where the end date is after 20xx/12/31...
             AND EndDate <= @EndDateDT2                  -- and before 20xx+1/01/14 <---if not, this will select all 20xx+1 batches
             AND BatchNumber > 19999999                  --only gives us batches/dates in the 20xxmmdd format
